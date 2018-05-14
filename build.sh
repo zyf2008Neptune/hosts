777d89ffb282f34bfe9d3f821b6eb357e403adc5
zyf2008Neptune <zyfrtas2008@gmail.com>
2018-05-14 19:21:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -30,9 +30,7 @@ else
 	echogr Importing SSH key.
 	base64 -d <<< $SSH_KEY > ~/.ssh/id_rsa
 	chmod 600 ~/.ssh/id_rsa
-	eval $(ssh-agent)
-	ssh-add ~/.ssh/id_rsa
-	ssh -T git@github.com
+	cp ssh_config ~/.ssh/config
 
 	echogr Cloning master branch.
 	git clone git@github.com:$TRAVIS_REPO_SLUG master
